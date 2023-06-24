68e3a9745dc8d85e8d746454bdebc4281568b42c
CharlieLit <136955810+CharlieLit@users.noreply.github.com>
2023-06-24 01:05:49 -0500

    Update config.js

--- config.js
+++ config.js
@@ -174,8 +174,8 @@ global.exito = eg
 
 //⊱ ━━━━━.⋅ Información | Information ⋅.━━━━ ⊰
 
-global.wm = 'Charlie'
-global.igfg = 'Charlie'
+global.wm = 'Kaz'
+global.igfg = 'Kaz'
 global.wait = '*⌛ _Cargando | Charging..._ ▬▭▭▭▭▭▭*'
 global.waitt = '*⌛ _Cargando | Charging..._ ▬▬▭▭▭*'
 global.waittt = '*⌛ _Cargando | Charging..._ ▬▬▬▬▭▭*'
